deaddba8d523d084aafdd670e092dba3a15603c1
Edgar Sánchez <esanchez>
2020-12-25 03:47:36 -0300

    Btns push down

--- yai-server/data/respaldos/Botones pushDown.cpp
+++ yai-server/data/respaldos/Botones pushDown.cpp
@@ -2,14 +2,29 @@
 
 //https://stackoverflow.com/questions/32135912/message-log-css-scroll-to-bottom
 
-int buttonState=0;
-int PD1 = 5;
-int PD2 = 4;
+bool buttonState = false;
+int pastState = 0;
+int buttonRead=0;
+int PD0 = 16;
+int PD1 = 5;  // SCL (I2C)
+int PD2 = 4;  // SDA (I2C)
 int PD3 = 0;
-int PD4 = 2;
-int PD5 = 14;
-int PD6 = 12;
+int PD4 = 2;  // TXD1
+int PD5 = 14; // SCLK
+int PD6 = 12; // MISO -> RDX2
+int PD7 = 13; // MOSI -> TXD2
+int PD8 = 15; // CS
+int PRX = 3; // RXD0
+int TRX = 1; // TXD0
 
+
+#define ButtonPin  PD4
+#define ButtonOn   LOW
+#define ButtonOff  HIGH
+
+#define RelayPin  PD5
+#define RelayOn   LOW
+#define RelayOff  HIGH
 
 void setup(void) {
 	Serial.begin(115200);
@@ -18,11 +33,19 @@ void setup(void) {
 }
 
 void loop(void) {
-  buttonState=digitalRead(PD4);
+  buttonRead=digitalRead(ButtonPin);
   // Reverse
-  if(buttonState == LOW) {
-    Serial.println("D4=" + String(buttonState)); 
-    Serial.println("D4 pressed");
-    delay(300);
+  if(buttonRead == LOW) {
+    if(pastState == HIGH) {
+      buttonState = buttonState?false:true;
+      pastState = LOW;
+      Serial.println("D4=" + String(buttonRead)); 
+      Serial.println("D4 pressed");
+      Serial.println("buttonState="+String(buttonState));
+    }
+  } else {
+    pastState = HIGH;
   }
+
+
 }
