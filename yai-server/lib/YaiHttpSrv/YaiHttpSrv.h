deee8b483453e1a13f85adc4b23a91ccf22a5520
Edgar Sánchez <esanchezd@ripley.com>
2020-12-26 00:20:05 -0300

    Rest

--- yai-server/lib/YaiHttpSrv/YaiHttpSrv.h
+++ yai-server/lib/YaiHttpSrv/YaiHttpSrv.h
@@ -3,6 +3,8 @@
 
 #include <ESPAsyncWebServer.h>
 #include <ESPAsyncTCP.h>
+#include <RestClient.h>
+
 
 //https://github.com/me-no-dev/ESPAsyncWebServer#respond-with-content-coming-from-a-stream
 
@@ -14,6 +16,10 @@ class YaiHttpSrv {
    
     YaiHttpSrv() {
       httpYaiServer = new AsyncWebServer(80);
+      String yaiIP = WiFi.localIP().toString();
+      char copyIp[yaiIP.length()];
+      yaiIP.toCharArray(copyIp, yaiIP.length());
+      restClient = new RestClient(copyIp, 5000);
     }
 
     void start();    
@@ -26,6 +32,7 @@ class YaiHttpSrv {
 
   private:
     AsyncWebServer* httpYaiServer;
+    RestClient* restClient;
 };
 
 #endif
