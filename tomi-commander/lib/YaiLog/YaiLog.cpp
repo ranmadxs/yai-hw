cc8f27ddfff202e54b884e9c7d117392683b43d2
Edgar Sanchez <grineldosanchez@yahoo.es>
2025-01-30 23:09:02 -0300

    0.0.31-ESP8266

--- tomi-commander/lib/YaiLog/YaiLog.cpp
+++ tomi-commander/lib/YaiLog/YaiLog.cpp
@@ -1,74 +1,55 @@
 #include "YaiLog.h"
+#if defined(ESP8266)
+  #include <ESP8266WiFi.h>
+  #include <ESP8266HTTPClient.h>
+#elif defined(ESP32)
+  #include <WiFi.h>
+  #include <HTTPClient.h>
+#endif
 
-// Definición de la función para obtener el tiempo en formato HH:MM:SS
-String getFormattedTime() {
-    unsigned long currentMillis = millis();
-    unsigned long totalSeconds = currentMillis / 1000;
-    unsigned int hours = (totalSeconds / 3600) % 24;
-    unsigned int minutes = (totalSeconds / 60) % 60;
-    unsigned int seconds = totalSeconds % 60;
+// Inicialización de la cola para ESP32
+#if defined(ESP32)
+QueueHandle_t YaiLog::sendQueue = NULL;
+#endif
+void serialAppender(String yrname, String msg, String levelName, const char* file, int line);
+String getFormattedTime();
 
-    char timeBuffer[9];  // Buffer para almacenar el tiempo formateado en HH:MM:SS
-    snprintf(timeBuffer, sizeof(timeBuffer), "%02d:%02d:%02d", hours, minutes, seconds);
-    return String(timeBuffer);  // Convertir a String y retornar
-}
-
-void serialAppender(String yrname, String msg, int level, const char* file, int line) {
-    String timestamp = getFormattedTime(); // Llamada a la función para obtener la hora
-
-    switch (level) {
-        case LOG_LEVEL_INFO:
-            Serial.print(ANSI_GREEN);  // Color verde para INFO
-            Serial.print("[INFO] ");
-            break;
-        case LOG_LEVEL_ERROR:
-            Serial.print(ANSI_RED);  // Color rojo para ERROR
-            Serial.print("[ERROR] ");
-            break;
-        case LOG_LEVEL_DEBUG:
-            Serial.print(ANSI_BLUE);  // Color azul para DEBUG
-            Serial.print("[DEBUG] ");
-            break;
-        case LOG_LEVEL_WARN:
-            Serial.print(ANSI_YELLOW);  // Color amarillo para WARN
-            Serial.print("[WARN] ");
-            break;
-        default:
-            Serial.print("[UNKNOWN LEVEL] ");
-            break;
+// Implementación de LogAppender::call
+void LogAppender::call(String yrname, String msg, String levelName, const char* file, int line) {
+    if (function) {
+        function(yrname, msg, levelName, file, line);
+    } else if (objectPtr && memberFunction) {
+        (objectPtr->*memberFunction)(yrname, msg, levelName, file, line);
     }
-
-    // Imprimir la marca de tiempo, el archivo, la línea, el nombre del logger y el mensaje
-    Serial.print(timestamp);
-    Serial.print(" - ");
-    Serial.print(file);  // Mostrar el archivo
-    Serial.print(":");
-    Serial.print(line);  // Mostrar la línea
-    Serial.print(" - ");
-    Serial.print(yrname);
-    Serial.print(": ");
-    Serial.println(msg);
-
-    // Resetear color
-    Serial.print(ANSI_RESET);  // Resetear al color por defecto
 }
 
-YaiLog::YaiLog() : totalAppender(0) {
+// Implementación de YaiLog
+
+YaiLog::YaiLog() : yrname("default"), totalAppender(0), logMessageCount(0), lastSendTime(millis()) {
     addAppender(LogAppender(serialAppender));
+#if SEND_LOG_TO_DATADOG
+    addAppender(LogAppender(this, &YaiLog::dataDogAppender));
+#endif
 }
 
-YaiLog::YaiLog(String yr_name) : yrname(yr_name), totalAppender(0) {
+YaiLog::YaiLog(String yr_name) : yrname(yr_name), totalAppender(0), logMessageCount(0), lastSendTime(millis()) {
     addAppender(LogAppender(serialAppender));
+#if SEND_LOG_TO_DATADOG
+    addAppender(LogAppender(this, &YaiLog::dataDogAppender));
+#endif
 }
 
 void YaiLog::addAppender(LogAppender appender) {
-    callbacks[totalAppender] = appender;
-    totalAppender++;
+    if (totalAppender < TOTAL_LOG_CALLBACKS) {
+        callbacks[totalAppender] = appender;
+        totalAppender++;
+    }
 }
 
 void YaiLog::baseLog(String str, int level, const char* file, int line) {
+    String levelStr = getLogLevelString(level);
     for (int i = 0; i < totalAppender; i++) {
-        callbacks[i].function(yrname, str, level, file, line); // Pasa el nombre, archivo, línea y nivel al appender
+        callbacks[i].call(yrname, str, levelStr, file, line);
     }
 }
 
@@ -87,3 +68,178 @@ void YaiLog::debug(String str, const char* file, int line) {
 void YaiLog::warn(String str, const char* file, int line) {
     baseLog(str, LOG_LEVEL_WARN, file, line);
 }
+
+String getFormattedTime() {
+    unsigned long currentMillis = millis();
+    unsigned long totalSeconds = currentMillis / 1000;
+    unsigned int hours = (totalSeconds / 3600) % 24;
+    unsigned int minutes = (totalSeconds / 60) % 60;
+    unsigned int seconds = totalSeconds % 60;
+
+    char timeBuffer[9];
+    snprintf(timeBuffer, sizeof(timeBuffer), "%02d:%02d:%02d", hours, minutes, seconds);
+    return String(timeBuffer);
+}
+
+String YaiLog::getLogLevelString(int level) {
+    switch(level) {
+        case LOG_LEVEL_INFO:
+            return "info";
+        case LOG_LEVEL_ERROR:
+            return "error";
+        case LOG_LEVEL_DEBUG:
+            return "debug";
+        case LOG_LEVEL_WARN:
+            return "warn";
+        case LOG_LEVEL_VERBOSE:
+            return "verbose";
+        default:
+            return "unknown";
+    }
+}
+
+// Appender para consola serial
+void serialAppender(String yrname, String msg, String levelName, const char* file, int line) {
+    String timestamp = getFormattedTime();
+    Serial.print("[" + levelName + "] ");
+    Serial.print(timestamp);
+    Serial.print("-");
+    Serial.print(file);
+    Serial.print(":");
+    Serial.print(line);
+    Serial.print("-");
+    Serial.print(yrname);
+    Serial.print(":");
+    Serial.println(msg);
+}
+
+void YaiLog::dataDogAppender(String yrname, String msg, String levelName, const char* file, int line) {
+    addLogMessage(msg, levelName);
+}
+
+void YaiLog::addLogMessage(String message, String level) {
+    if (logMessageCount < MAX_LOG_MESSAGES) {
+        LogMessage& log = logMessages[logMessageCount++];
+        log.message = message;
+        log.level = level;
+        log.service = yrname;
+        log.ddsource = "arduino";
+        log.hostname = WiFi.localIP().toString();
+        log.tags = "env:production";
+        log.date = getFormattedTime();  // Usando el tiempo formateado para simplicidad
+    }
+}
+
+void YaiLog::loop() {
+    this->sendDataDogLogs();
+}
+
+// Implementación de sendDataDogLogs
+void YaiLog::sendDataDogLogs() {
+    unsigned long currentTime = millis();
+    // Verificar si es hora de enviar los logs
+    if ((currentTime - lastSendTime >= 3 * 60 * 1000) || (logMessageCount >= MAX_LOG_MESSAGES)) {
+        sendToDatadogAsync();
+        lastSendTime = currentTime;
+    }
+}
+
+void YaiLog::sendToDatadogAsync() {
+    if (!SEND_LOG_TO_DATADOG_FLAG) {
+        return;
+    }
+
+#if defined(ESP32)
+    // Para ESP32, enviar una tarea a la cola
+    if (sendQueue == NULL) {
+        sendQueue = xQueueCreate(10, sizeof(SendTaskParams));
+        if (sendQueue == NULL) {
+            Serial.println("YaiLog :]> Error creando la cola para sendToDatadogAsync");
+            return;
+        }
+        // Crear la tarea de envío
+        if (xTaskCreatePinnedToCore(sendTask, "SendTask", 4096, this, 1, NULL, 1) != pdPASS) {
+            Serial.println("YaiLog :]> Error creando la tarea para sendToDatadogAsync");
+            return;
+        }
+    }
+
+    // Crear los parámetros de la tarea
+    SendTaskParams taskParams;
+    taskParams.instance = this;
+
+    // Enviar los parámetros a la cola
+    if (xQueueSend(sendQueue, &taskParams, portMAX_DELAY) != pdPASS) {
+        Serial.println("YaiLog :]> Error: No se pudo añadir la tarea a la cola para sendToDatadogAsync.");
+    }
+
+#elif defined(ESP8266)
+    // Para ESP8266, usar Ticker para llamar a sendDataDogLogs de forma asíncrona
+    sendMetricTicker.once_ms(100, [this]() { this->sendDataDogLogsAsync(); });
+#endif
+}
+
+void YaiLog::sendDataDogLogsAsync() {
+    if (logMessageCount == 0) {
+        return;
+    }
+
+    HTTPClient http;
+    WiFiClient client;
+    http.begin(client, "https://tomi-metric-collector-production.up.railway.app/logs");    
+    //http.begin("https://tomi-metric-collector-production.up.railway.app/logs");
+    http.addHeader("Content-Type", "application/json");
+
+    // Construir el payload JSON
+    String payload = "{\"logs\":[";
+    for (int i = 0; i < logMessageCount; i++) {
+        LogMessage& log = logMessages[i];
+        payload += "{";
+        payload += "\"message\":\"" + log.message + "\",";
+        payload += "\"level\":\"" + log.level + "\",";
+        payload += "\"service\":\"" + log.service + "\",";
+        payload += "\"ddsource\":\"" + log.ddsource + "\",";
+        payload += "\"hostname\":\"" + log.hostname + "\",";
+        //payload += "\"date\":\"" + log.date + "\",";
+        payload += "\"tags\":[\"" + log.tags + "\"]";
+
+        payload += "}";
+        if (i < logMessageCount - 1) {
+            payload += ",";
+        }
+    }
+    payload += "]}";
+
+    int httpResponseCode = http.POST(payload);
+
+    if (httpResponseCode > 0) {
+        String response = http.getString();
+        // Manejar la respuesta si es necesario
+        Serial.println("YaiLog :]> Send to Datadog: logMessageCount=" + String(logMessageCount));
+    } else {
+        // Manejar error
+        Serial.println("YaiLog :]> Error al enviar logs a tomiCollector: " + String(httpResponseCode));
+    }
+    http.end();
+
+    // Reiniciar el contador de mensajes después de enviar
+    logMessageCount = 0;
+}
+
+#if defined(ESP32)
+  // Implementación de la tarea de envío para ESP32
+  void YaiLog::sendTask(void* parameter) {
+      YaiLog* logger = (YaiLog*) parameter;
+      while (1) {
+          // Esperar a que haya una tarea en la cola
+          SendTaskParams params;
+          if (xQueueReceive(sendQueue, &params, portMAX_DELAY)) {
+              if (params.instance) {
+                  params.instance->sendDataDogLogsAsync();
+              }
+          }
+          // Añadir un pequeño retraso para evitar el bloqueo
+          vTaskDelay(10 / portTICK_PERIOD_MS);
+      }
+  }
+#endif
--- tomi-commander/lib/YaiLog/YaiLog.h
+++ tomi-commander/lib/YaiLog/YaiLog.h
@@ -3,17 +3,25 @@
 
 #include <Arduino.h>
 #include <logger.h>
+#include "YaiWIFI.h"
+#if defined(ESP32)
+  #include <freertos/FreeRTOS.h>
+  #include <freertos/queue.h>
+  #include <freertos/task.h>
+#elif defined(ESP8266)
+  #include <Ticker.h>
+#endif
 
 #define TOTAL_LOG_CALLBACKS 10
 
-// Definiciones de los niveles de log
-const u_int16_t LOG_LEVEL_INFO = 1;
-const u_int16_t LOG_LEVEL_ERROR = 2;
-const u_int16_t LOG_LEVEL_DEBUG = 3;
-const u_int16_t LOG_LEVEL_WARN = 4;
-const u_int16_t LOG_LEVEL_VERBOSE = 5;
+// Definición de los niveles de log
+const uint16_t LOG_LEVEL_INFO = 1;
+const uint16_t LOG_LEVEL_ERROR = 2;
+const uint16_t LOG_LEVEL_DEBUG = 3;
+const uint16_t LOG_LEVEL_WARN = 4;
+const uint16_t LOG_LEVEL_VERBOSE = 5;
 
-// Definiciones de colores ANSI para la consola serial
+// Definiciones de colores ANSI para la consola serial (vacío)
 #define ANSI_RESET  ""
 #define ANSI_BLUE   ""
 #define ANSI_RED    ""
@@ -26,12 +34,23 @@ const u_int16_t LOG_LEVEL_VERBOSE = 5;
 #define LOG_ERROR(logger, msg) (logger).error((msg), __FILE__, __LINE__)
 #define LOG_WARN(logger, msg) (logger).warn((msg), __FILE__, __LINE__)
 
+// Definir si se envían logs a DataDog
+#define SEND_LOG_TO_DATADOG 1
+
+class YaiLog;  // Declaración adelantada
+
 class LogAppender {
   public:
     ~LogAppender() {}
-    LogAppender(void (*f)(String, String, int, const char*, int) = 0)
-        : function(f) {}
-    void (*function)(String, String, int, const char*, int);
+    LogAppender(void (*f)(String, String, String, const char*, int) = 0)
+        : function(f), objectPtr(nullptr), memberFunction(nullptr) {}
+    LogAppender(YaiLog* obj, void (YaiLog::*mf)(String, String, String, const char*, int))
+        : function(nullptr), objectPtr(obj), memberFunction(mf) {}
+    void call(String yrname, String msg, String levelName, const char* file, int line);
+  private:
+    void (*function)(String, String, String, const char*, int);
+    YaiLog* objectPtr;
+    void (YaiLog::*memberFunction)(String, String, String, const char*, int);
 };
 
 class YaiLog {
@@ -43,14 +62,55 @@ class YaiLog {
     void debug(String str, const char* file, int line);
     void warn(String str, const char* file, int line);
     void addAppender(LogAppender appender);
+    void loop();
+    String getLogLevelString(int level);
+    // Nuevo método para enviar logs asíncronamente a DataDog
+    void sendToDatadogAsync();
 
   protected:
+    boolean SEND_LOG_TO_DATADOG_FLAG = SEND_LOG_TO_DATADOG;
     String yrname;
     LogAppender callbacks[TOTAL_LOG_CALLBACKS];
     int totalAppender;
 
+    // Estructura para representar un mensaje de log
+    struct LogMessage {
+        String message;
+        String level;
+        String service;
+        String ddsource;
+        String hostname;
+        String tags;
+        String date;
+    };
+
+    // Métodos y miembros para el appender de DataDog
+    void dataDogAppender(String yrname, String msg, String levelName, const char* file, int line);
+    void sendDataDogLogs();
+    void addLogMessage(String message, String level);
+    static const int MAX_LOG_MESSAGES = 100;
+    LogMessage logMessages[MAX_LOG_MESSAGES];
+    int logMessageCount;
+    unsigned long lastSendTime;
+
+    // Nuevo: Método para enviar los logs desde una tarea o ticker
+    void sendDataDogLogsAsync();
+
   private:
     void baseLog(String str, int level, const char* file, int line);
+
+#if defined(ESP32)
+    // Estructuras y variables para ESP32
+    struct SendTaskParams {
+        YaiLog* instance;
+    };
+    static QueueHandle_t sendQueue;
+    static void sendTask(void* parameter);
+#elif defined(ESP8266)
+    // Instancia de Ticker para ESP8266
+    Ticker sendMetricTicker;
+    void sendToDatadog();
+#endif
 };
 
-#endif
+#endif  // YaiLog_h
